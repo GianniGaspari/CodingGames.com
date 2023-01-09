9a026f5df1f00d40da977546073c4bbf31e2b081
Gianni Gaspari <gianni.gaspari.s@gmail.com>
2023-01-09 04:06:11 -0300

    initial commit

---
+++ Practice/CodingGame002.py
@@ -0,0 +1,15 @@
+import sys
+import math
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+n = int(input())
+
+# Write an answer using print
+# To debug: print("Debug messages...", file=sys.stderr, flush=True)
+
+if n % 4 == 0:
+    print("AAAAAAAAAA!!!")
+else:
+    print("Ok")
